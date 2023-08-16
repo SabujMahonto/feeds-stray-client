e145482560ea87a873277d3e31760d361d701239
sabujmahonto <sabujmahonto@gmail.com>
2023-08-16 12:56:39 +0600

    ourvision done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Consumers from "@/components/Consumers";
 import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
+import OurVision from "@/components/OurVision";
 import Providers from "@/components/Providers";
 import Testimonials from "@/components/Testimonials";
 
@@ -8,6 +9,7 @@ const HomePage = () => {
   return (
     <main className="mt-16">
       <Hero />
+      <OurVision />
       <Providers />
       <Consumers />
       <Gallery />
