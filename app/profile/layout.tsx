b43e7e178c14de9fa77ede6ff19b2414bc1bd1df
sabujmahonto <sabujmahonto@gmail.com>
2023-08-16 12:49:46 +0600

    register page doen

---
+++ app/profile/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "FeedStray | Profile",
+};
+
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
